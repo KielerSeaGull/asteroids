1729ebb3296526b6e2a70b548252ea9d7d9846b1
KielerSeaGull <joel@blaich.rocks>
2024-12-22 19:13:04 +0100

    added playermodul with draw function. Play will be rendered in main()

--- main.py
+++ main.py
@@ -3,18 +3,31 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
     print(f"Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
+        player1.draw(screen)
+
         pygame.display.flip()
+        # 60 FPS is set
+        clock.tick(60)
+        # time in sec passed since the last tick (delta time)
+        dt = clock.tick()/1000
 
 if __name__ == "__main__":
     main()
